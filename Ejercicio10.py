39bfdfd1f63c44eb74ec87613de585ae6cdfbc72
jotano <jotanosanm@educacion.navarra.es>
2023-11-03 00:07:49 +0100

    Ejercicio 10

---
+++ Ejercicio10.py
@@ -0,0 +1,6 @@
+lista_precios = [50, 75, 46, 22, 80, 65, 8]
+precio_bajo = min(lista_precios)
+precio_alto = max(lista_precios)
+print("El precio más alto es:", precio_alto)
+print("El precio más bajo es:", precio_bajo)
+input("Pulsa enter para salir")
