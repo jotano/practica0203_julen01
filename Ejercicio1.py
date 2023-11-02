f21b021a9d7e897818b48cfc1223079668b9e02a
jotano <jotanosanm@educacion.navarra.es>
2023-11-02 18:21:06 +0100

    Ejercicio1

---
+++ Ejercicio1.py
@@ -0,0 +1,5 @@
+Asignaturas = ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
+print("Asignaturas:")
+for asignatura in Asignaturas:
+    print(asignatura)
+input("Pulsa enter para salir")
